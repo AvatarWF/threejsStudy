cd49831c0550211727ad3b5ce32ca2ab1391ab26
Fernando Filho <you@example.com>
2024-07-29 16:30:23 -0300

    first commit

---
+++ scenes/scene-01.js
@@ -0,0 +1,17 @@
+const geometry = new THREE.BoxBufferGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF });
+
+
+const cube = new THREE.Mesh(
+  geometry, material
+);
+
+scene.add(cube);
+
+renderer.setAnimationLoop(() => {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+});
+
