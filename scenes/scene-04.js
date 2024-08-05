7d4604f0f1c8f460554b3dffca01e84759821de4
Fernando Filho <you@example.com>
2024-08-05 13:57:28 -0300

    Point Light whithout shadows

--- scenes/scene-04.js
+++ scenes/scene-04.js
@@ -1,4 +1,27 @@
 
+const cube = new THREE.Mesh(
+  new THREE.BoxBufferGeometry(),
+  new THREE.MeshLambertMaterial({ color: 0x368ed1 })
+);
+
+cube.position.x = 1;
+cube.position.y = 1;
+scene.add(cube);
+
+const floor = new THREE.Mesh(
+  new THREE.PlaneBufferGeometry(10, 10),
+  new THREE.MeshLambertMaterial({ color: 0xFFFFFF, side: THREE.DoubleSide })
+)
+// floor.position.y = 0.01;
+floor.rotation.x = THREE.MathUtils.degToRad(-90);
+scene.add(floor);
+
+const shadowLight = new THREE.PointLight(0xff0000);
+shadowLight.position.y = 4;
+scene.add(shadowLight);
+
+x3.add(shadowLight);
+
 renderer.setAnimationLoop(() => {
   x3.tick();
   x3.fps(() => {
